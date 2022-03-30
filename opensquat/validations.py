2bb20650acbc9e117a7a493e35e32534244711db
Anonymous <102689204+OperationAnonymous@users.noreply.github.com>
2022-03-30 14:52:16 -0400

    Create validations.py

---
+++ opensquat/validations.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Module: validations.py
+"""
+openSquat.
+
+(c) OperationAnonymous
+
+* https://github.com/OperationAnonymous
+"""
+from strsimpy.levenshtein import Levenshtein
+from strsimpy.jaro_winkler import JaroWinkler
+
+
+def levenshtein(keyword, domain):
+    """
+    Compute Levenshtein distance.
+
+    Args:
+        keyword:
+        domain:
+
+    Return:
+        leven.distance: Levenshtein Distance (int)
+    """
+    leven = Levenshtein()
+    return leven.distance(keyword, domain)
+
+
+def jaro_winkler(keyword, domain):
+    """
+    Compute Jaro Winkler similarity.
+
+    Args:
+        keyword:
+        domain:
+
+    Return:
+        jarowinkler.similarity: (float) between 0.0 and 1.0
+    """
+    jarowinkler = JaroWinkler()
+    return jarowinkler.similarity(keyword, domain)
